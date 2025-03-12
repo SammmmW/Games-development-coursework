5c91e44d2c095a6cc5496d6400cfa2e01b2af02e
SammmmW <158096243+SammmmW@users.noreply.github.com>
2025-03-12 11:02:28 +0000

    Add files via upload
    Updated scipts

---
+++ Scripts/Tank/StationaryTank.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StationaryTank : MonoBehaviour
+{
+    public Transform Target;
+    public float attackRange;
+    public NavMeshAgent m_Agent;
+    private float m_Distance;
+    public int timer = 0;
+    public bool hasFired = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Distance = Vector3.Distance(m_Agent.transform.position, Target.position);
+        if (m_Distance < attackRange)
+        {
+            if (!hasFired)
+            {
+                AITankShooting shot = m_Agent.GetComponent<AITankShooting>();
+                shot.Fire();
+                hasFired = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Idle");
+        }
+    }
+}
